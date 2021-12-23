cb34815d613cefe8e244455303152a783d378fcd
KariUlrich <kariulrich@hotmail.com>
2021-12-22 22:27:51 -0300

    armando secciones

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//Get
+// definicion de variables
+
+const botonCharacter = document.querySelector(".boton-character")
+const seccionSearch = document.querySelector(".seccion-busqueda")
+const seccionHeader = document.querySelector(".seccion-header")
+const seccionCharacter = document.querySelector(".seccion-character")
+
+// pagina principal
+
+seccionSearch.style.display = "none"; 
+seccionCharacter.style.display = "none";
+
+
+// onclic de los botones de navegacion
+
+botonCharacter.onclick = () => {
+    seccionSearch.style.display = "flex"; 
+    seccionHeader.style.display = "none";
+    seccionCharacter.style.display = "flex";
+
+}
